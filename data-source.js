51444d2c8317e1989686c39c02ab85de525ac9db
kuldip <kuldiplohare80@gmail.com>
2025-06-19 17:15:19 +0530

    Fix database path for Render

--- data-source.js
+++ data-source.js
@@ -2,15 +2,9 @@ const { DataSource } = require('typeorm');
 const path = require('path');
 require('dotenv').config();
 
-try {
-  require('better-sqlite3'); // 👈 ensures it's loaded
-} catch (err) {
-  console.error("❌ Failed to load better-sqlite3:", err);
-}
-
 const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: process.env.DB_PATH || path.join('/tmp', 'database.sqlite'),
+  database: process.env.DATABASE || path.join('/tmp', 'sqlite.db'),
   synchronize: true,
   entities: [path.join(__dirname, './entity/*.js')],
 });
